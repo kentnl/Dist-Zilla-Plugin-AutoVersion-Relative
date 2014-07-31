9717a19598fbb85e9c6b6794cd7f4b7492a24468
Kent Fredric <kentfredric@gmail.com>
2014-07-31 17:33:51 +1200

    Build results of f02ea60 (on master)

--- lib/Dist/Zilla/Plugin/AutoVersion/Relative.pm
+++ lib/Dist/Zilla/Plugin/AutoVersion/Relative.pm
@@ -54,7 +54,7 @@ our $AUTHORITY = 'cpan:KENTNL'; # AUTHORITY
 
 
 
-use Moose 1.09 qw( has around );
+use Moose 1.09 qw( has around with );
 use MooseX::Types::Moose qw( :all );
 use MooseX::Types::DateTime qw( TimeZone Duration Now );
 use MooseX::StrictConstructor 0.10;
