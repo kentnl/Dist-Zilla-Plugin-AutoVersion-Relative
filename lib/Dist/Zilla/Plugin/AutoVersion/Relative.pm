6c83a6438cd1de8121cb622086f415ec2f4c3ec1
Kent Fredric <kentfredric@gmail.com>
2014-08-16 12:03:16 +1200

    Build results of 02363ad (on master)

--- lib/Dist/Zilla/Plugin/AutoVersion/Relative.pm
+++ lib/Dist/Zilla/Plugin/AutoVersion/Relative.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::AutoVersion::Relative;
 
-our $VERSION = '1.000001';
+our $VERSION = '1.000002';
 
 # ABSTRACT: Time-Relative versioning
 
@@ -276,7 +276,7 @@ Dist::Zilla::Plugin::AutoVersion::Relative - Time-Relative versioning
 
 =head1 VERSION
 
-version 1.000001
+version 1.000002
 
 =head1 SYNOPSIS
 
